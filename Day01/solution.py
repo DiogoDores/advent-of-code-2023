a63a3e66682a948008db40c80342f4f8eeb7890d
Diogo Dores <diogo.dores@gmail.com>
2023-12-02 01:47:27 +0000

    Add Day 1 solution

--- Day01/solution.py
+++ Day01/solution.py
@@ -1,1 +1,23 @@
 f = open("input.txt", "r")
+input = [line for line in f.readlines()]
+
+def data_calibration(input):
+    calibrationValues = []
+    for line in input:
+        if not line.strip().isalpha():
+            calibrationValues.append(int(''.join([[digit for digit in line if digit.isdigit()][0], [digit for digit in line[::-1] if digit.isdigit()][0]])))
+    return calibrationValues
+
+print(f'Part 1: {sum(data_calibration(input))}')
+
+input = [line.replace("one", "one1one")
+        .replace("two", "two2two")
+        .replace("three", "three3three")
+        .replace("four", "four4four")
+        .replace("five", "five5five")
+        .replace("six", "six6six")
+        .replace("seven", "seven7seven")
+        .replace("eight", "eight8eight")
+        .replace("nine", "nine9nine") for line in input]
+
+print(f'Part 2: {sum(data_calibration(input))}')
